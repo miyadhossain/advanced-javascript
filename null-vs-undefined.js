43546ca97b39b3c5e31a7a4d20b535c67a6e644f
Md Miyad Hossain <miyad.mh@gmail.com>
2021-02-02 12:09:47 +0600

    first commit

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+let pakhi;
+function add(num1, num2) {
+    console.log(num1+num2);
+    return
+}
+
+function add(a,b) {
+    console.log(a+b);
+}
+const result = add(5)
+
+const premik = {name: 'manza Rafique', phone: 25478990}
+console.log(premik.gfname);
+
+let ages = [25,63,74];
+console.log(ages[10]);
